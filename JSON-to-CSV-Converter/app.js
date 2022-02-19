f121230680d5ad6b708cb1df4a075b0aa0c1c159
Westen0511 <xiyuanjunzhi@gmail.com>
2022-02-19 14:56:37 +0900

    feat: Add toggle input/output view

--- JSON-to-CSV-Converter/app.js
+++ JSON-to-CSV-Converter/app.js
@@ -7,16 +7,29 @@ conversionBtn.addEventListener("click", (e) => {
     // 変換処理終了(ローディングアニメーション終了)
 })
 
-function load() {
-    const loading = document.getElementById("conversion-loading");
+const loading = document.getElementById("conversion-loading");
+const outputContainer = document.getElementById("output-container");
+const inputContainer = document.getElementById("input-container");
+const defaultCheckedOutputTab = document.getElementById("output-csv-tab");
+const tabs = document.querySelectorAll("input[name='tab']");
 
-    function startLoad() {
-        loading.style.visibility = "visible";
-    };
-    function stopLoad() {
-        loading.style.visibility = "hidden";
+function startLoad() {
+    loading.style.visibility = "visible";
+};
+
+function stopLoad() {
+    inputContainer.style.display = "none";
+    outputContainer.style.display = "flex";
+
+    for (const tab of tabs) {
+        tab.checked = false
     }
+    defaultCheckedOutputTab.checked = true;
 
+    loading.style.visibility = "hidden";
+};
+
+function load() {
     startLoad();
     setTimeout(stopLoad, 3000);
 }
