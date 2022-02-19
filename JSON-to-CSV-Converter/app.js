683f90a79799df6a7ef760869a5e2bd3500b582e
Westen0511 <xiyuanjunzhi@gmail.com>
2022-02-19 17:03:56 +0900

    feat: Add input validation

--- JSON-to-CSV-Converter/app.js
+++ JSON-to-CSV-Converter/app.js
@@ -1,35 +1,91 @@
-const conversionBtn = document.getElementById("conversion-btn");
-conversionBtn.addEventListener("click", (e) => {
-    console.log(e);
-    load()
-    // 変換処理開始(ローディングアニメーション表示)
-    // 変換処理実行
-    // 変換処理終了(ローディングアニメーション終了)
-})
-
+// 表示切り替え
 const loading = document.getElementById("conversion-loading");
 const outputContainer = document.getElementById("output-container");
 const inputContainer = document.getElementById("input-container");
-const defaultCheckedOutputTab = document.getElementById("output-csv-tab");
+// タブ
+const inputJSONTab = document.getElementById("input-json-tab");
+const uploadJSONTab = document.getElementById("upload-json-tab");
+const outputCSVTab = document.getElementById("output-csv-tab");
+const downloadCSVTab = document.getElementById("download-csv-tab");
 const tabs = document.querySelectorAll("input[name='tab']");
+// ボタン
+const conversionBtn = document.getElementById("conversion-btn");
+// 入力ファイル
+const fileInput = document.getElementById("input-json-file");
+// ファイル読み込み結果
+let readResult = ""
 
+
+conversionBtn.addEventListener("click", (e) => {
+    // 変換処理開始(ローディングアニメーション表示)
+    startLoad();
+    // 変換処理実行
+    validateResult = validateInput()
+    if (!validateResult) return
+    console.log(validateResult)
+    // 変換処理終了(ローディングアニメーション終了)
+    stopLoad();
+})
+
+// ------------------------------------
+// 入力
+// ------------------------------------
+/** テキストが入力された */
+function validateInput() {
+    const inputText = document.getElementById("input-json-text").value;
+
+    if (inputText && readResult) {
+        window.alert("入力はテキストかファイルのどちらかにしてください。")
+        return false
+    } else if (inputText) {
+        return inputText
+    } else if (readResult) {
+        return readResult
+    } else {
+        window.alert("テキストかファイルを入力してください。")
+        return false
+    }
+}
+
+
+function readFile() {
+    const reader = new FileReader();
+    reader.readAsText(fileInput.files[0])
+    reader.addEventListener("load", () => {
+        readResult = reader.result;
+    })
+}
+fileInput.addEventListener("change", readFile);
+
+
+// ------------------------------------
+// 表示
+// ------------------------------------
+/** ロード開始時の処理 */
 function startLoad() {
     loading.style.visibility = "visible";
 };
 
+/** ロード終了時の処理 */
 function stopLoad() {
-    inputContainer.style.display = "none";
-    outputContainer.style.display = "flex";
+    toggleDisplay(inputContainer);
+    toggleDisplay(outputContainer);
 
     for (const tab of tabs) {
         tab.checked = false
     }
-    defaultCheckedOutputTab.checked = true;
+    outputCSVTab.checked = true;
 
     loading.style.visibility = "hidden";
 };
 
-function load() {
-    startLoad();
-    setTimeout(stopLoad, 3000);
+/** 要素の表示を切り替える */
+function toggleDisplay(element) {
+    if (element.classList.value === "active") {
+        element.classList.remove("active");
+        element.classList.add("inactive");
+    } else {
+        element.classList.remove("inactive");
+        element.classList.add("active");
+    }
 }
