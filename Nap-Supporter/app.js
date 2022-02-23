6a05ab3ae1491bb8655a2c83b6ac864b59eb2e91
Westen0511 <xiyuanjunzhi@gmail.com>
2022-02-23 17:22:07 +0900

    Refactor: Add correspondence.correspondInputTimer

--- Nap-Supporter/app.js
+++ Nap-Supporter/app.js
@@ -13,6 +13,10 @@
  * bgmSelect, alarmSelect をbindでリファクタリング
  * イベントリスナーにおいて、thisはwindowを指す。
  * →bindを使ってthisの参照をselect要素にすることで対応。
+ * https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+ *
+ * ついでに、this使えばcorrespondenceもリファクタリング出来ること無い？
+ * 　
  */
 
 /** 選択された音声のHTML要素 */
@@ -30,13 +34,32 @@ const volumes = {
 const correspondenceInputMinutesTimer = {
     "timer" : document.getElementById("timer-minutes"),
     "range" : document.getElementById("minutes-range"),
-    "number" : document.getElementById("minutes-number")
+    "number" : document.getElementById("minutes-number"),
+    /** input ⇔ timerを紐付ける */
+    correspondInputTimer() {
+        // numberは値が表示されるため、0埋めしておく
+        this.number.value = this.number.value.padStart(2, "0");
+        this.timer.textContent = this.number.value;
+
+        this.range.addEventListener("input", () => {
+            // rangeの値を0埋めしてnumberに代入する
+            this.number.value = this.range.value.padStart(2, "0");
+            this.timer.textContent = this.range.value.padStart(2, "0");
+        })
+        this.number.addEventListener("input", () => {
+            // rangeにはそのまま値を代入すればいい
+            this.range.value = this.number.value;
+            this.number.value = this.number.value.padStart(2, "0");
+            this.timer.textContent = this.number.value
+        })
+    }
 }
 /** input⇔秒タイマーの紐付け */
 const correspondenceInputSecondsTimer = {
+    ...correspondenceInputMinutesTimer,
     "timer" : document.getElementById("timer-seconds"),
     "range" : document.getElementById("seconds-range"),
-    "number" : document.getElementById("seconds-number")
+    "number" : document.getElementById("seconds-number"),
 }
 
 const bgmSelect = document.getElementById("bgm-select");
@@ -46,54 +69,25 @@ const startBtn = document.getElementById("start-btn");
 const stopBtn = document.getElementById("stop-btn");
 const bgmVolumeCheck = document.getElementById("bgm-volume-check");
 const alarmVolumeCheck = document.getElementById("alarm-volume-check");
-const buttonsDisabledPlayback = [startBtn, bgmVolumeCheck, alarmVolumeCheck];
+
+/** 再生時無効にするHTML要素 */
+const elementsDisabledPlayback = [startBtn, bgmVolumeCheck, alarmVolumeCheck];
 
 let isStopClick = false;
 
 /** 音声が選択された時 */
 function selectSound(audioId) {
-    console.log(this)
-    const index = this.selectedIndex;
-    const dataValue = this.options[index].dataset[audioId];
+    // カスタムデータ属性の取得 element.options.index.dataset.key
+    const dataValue = this.options[this.selectedIndex].dataset[audioId];
     selectedSounds[audioId] = document.getElementById(`${audioId}-${dataValue}`);
 }
-
-// bindでthisの参照をselect要素にする
-bgmSelect.addEventListener("change", selectSound.bind(bgmSelect, "bgm"));
-alarmSelect.addEventListener("change", selectSound.bind(alarmSelect, "alarm"));
-
-
-/** inputとtimerを紐付ける */
-function correspondInputTimer(correspond) {
-    // numberは値が表示されるため、0埋めしておく
-    correspond.number.value = correspond.number.value.padStart(2, "0");
-    correspond.timer.textContent = correspond.number.value;
-
-    correspond.range.addEventListener("input", () => {
-        // rangeの値を0埋めしてnumberに代入する
-        correspond.number.value = correspond.range.value.padStart(2, "0");
-        correspond.timer.textContent = correspond.range.value.padStart(2, "0");
-    })
-
-    correspond.number.addEventListener("input", () => {
-        // rangeにはそのまま値を代入すればいい
-        correspond.range.value = correspond.number.value;
-        correspond.number.value = correspond.number.value.padStart(2, "0");
-        correspond.timer.textContent = correspond.number.value
-    })
-}
-
-correspondInputTimer(correspondenceInputMinutesTimer);
-correspondInputTimer(correspondenceInputSecondsTimer);
-
-
 
 /** audio要素をplayし、ボタンを無効化する */
 function audioPlay(audio, volume) {
     audio.volume = volume;
     audio.loop = true;
     audio.play();
-    buttonsDisabledPlayback.forEach(btn => {
+    elementsDisabledPlayback.forEach(btn => {
         btn.disabled = true;
     });
 }
@@ -102,7 +96,7 @@ function audioPlay(audio, volume) {
 function audioPause(audio) {
     audio.pause();
     audio.currentTime = 0;
-    buttonsDisabledPlayback.forEach(btn => {
+    elementsDisabledPlayback.forEach(btn => {
         btn.disabled = false;
     });
 }
@@ -111,6 +105,13 @@ function audioPause(audio) {
 function audioPlayShort(audio, volume) {
     audioPlay(audio, volume);
     setTimeout(audioPause, 5000, audio);
+}
+
+/** 音量チェックを一定時間再生する。 */
+function playVolumeCheck(audioId) {
+    const selectedAudio = selectedSounds[audioId];
+    const volume = volumes[audioId].value / 100;
+    audioPlayShort(selectedAudio, volume);
 }
 
 /** タイマーの入力値をセットする */
@@ -132,7 +133,7 @@ function start() {
     function finishPlay() {
         clearInterval(id);
         setTimerInputs(min, sec);
-        audioPause(selectedSounds.bgm, buttonsDisabledPlayback);
+        audioPause(selectedSounds.bgm, elementsDisabledPlayback);
     }
 
     // 2. 音声を再生する
@@ -163,21 +164,15 @@ function start() {
     }, 1000);
 }
 
-startBtn.addEventListener("click", start)
-stopBtn.addEventListener("click", () => {
-    isStopClick = true;
-})
+correspondenceInputMinutesTimer.correspondInputTimer()
+correspondenceInputSecondsTimer.correspondInputTimer()
 
-// ------------------------------------------
-// 音量チェック
-// ------------------------------------------
-/** 音量チェックを一定時間再生する。 */
-function playVolumeCheck(audioId) {
-    const selectedAudio = selectedSounds[audioId];
-    // 入力レンジが0〜100でボリュームは0〜1なので100で割る
-    const volume = volumes[audioId].value / 100;
-    audioPlayShort(selectedAudio, volume);
-}
-
+// bindでthisの参照をselect要素にする
+bgmSelect.addEventListener("change", selectSound.bind(bgmSelect, "bgm"));
+alarmSelect.addEventListener("change", selectSound.bind(alarmSelect, "alarm"));
+// thisが不要の場合はbindにnullを渡す
 bgmVolumeCheck.addEventListener('click', playVolumeCheck.bind(null, "bgm"))
 alarmVolumeCheck.addEventListener('click', playVolumeCheck.bind(null, "alarm"))
+
+startBtn.addEventListener("click", start)
+stopBtn.addEventListener("click", () => isStopClick = true )
