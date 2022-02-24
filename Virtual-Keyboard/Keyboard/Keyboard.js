8bc43949758fd4a65177118413a855d5728ac8cf
Westen0511 <xiyuanjunzhi@gmail.com>
2022-02-24 20:10:41 +0900

    feat: Add keyboard minimum functions

---
+++ Virtual-Keyboard/Keyboard/Keyboard.js
@@ -0,0 +1,18 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        // キーボードの入力
+        value: "",
+        capsLock: false
+    }
+};
