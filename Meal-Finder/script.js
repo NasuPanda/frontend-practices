56185d6581cf5ef05a115c216e7ce5e6641efd0f
Westen0511 <xiyuanjunzhi@gmail.com>
2022-02-27 15:19:32 +0900

    finish

--- Meal-Finder/script.js
+++ Meal-Finder/script.js
@@ -48,12 +48,26 @@ function searchMeal(e) {
 // Fetch meal by ID
 function getMealByID(mealID) {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
-    .then(res => res.json())
-    .then(data => {
-        const meal = data.meals[0];
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
 
-        addMealToDOM(meal);
+            addMealToDOM(meal);
     })
+}
+
+// Fetch random meal from API
+function getRandomMeal() {
+    mealsEl.innerHTML = "";
+    resultHeading.innerHTML = "";
+
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+
+            addMealToDOM(meal);
+        })
 }
 
 // Add meal to DOM
@@ -89,6 +103,7 @@ function addMealToDOM(meal) {
 
 // Event listeners
 submit.addEventListener("submit", searchMeal);
+random.addEventListener("click", getRandomMeal);
 
 mealsEl.addEventListener("click", e=> {
     const mealInfo = e.path.find(item => {
