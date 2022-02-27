458727b0d791c02070fbaf546618e3944f5e12d3
Westen0511 <xiyuanjunzhi@gmail.com>
2022-02-27 15:00:47 +0900

    style: Add meal info styles

--- Meal-Finder/script.js
+++ Meal-Finder/script.js
@@ -71,7 +71,7 @@ function addMealToDOM(meal) {
         <div class="single-meal">
             <h1>${meal.strMeal}</h1>
             <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-            <div class="single-meal-infl">
+            <div class="single-meal-info">
                 ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
                 ${meal.strArea ? `<p>${meal.strCategory}</p>` : ""}
             </div>
