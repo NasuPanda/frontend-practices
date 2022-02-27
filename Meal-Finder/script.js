2d7f09c6136151f4f68bd91522d8b7d88466a64d
Westen0511 <xiyuanjunzhi@gmail.com>
2022-02-27 10:13:27 +0900

    feat: Add show search results

--- Meal-Finder/script.js
+++ Meal-Finder/script.js
@@ -1,3 +1,5 @@
+// https://www.themealdb.com/api.php
+
 const search = document.getElementById("search"),
     submit = document.getElementById("submit"),
     random = document.getElementById("random"),
@@ -20,8 +22,24 @@ function searchMeal(e) {
             .then(res => res.json())
             .then(data => {
                 console.log(data);
-                resultHeading.innerHTML = `<h2>Search results for: ${term}</h2>`;
+                resultHeading.innerHTML = `<h2>Search results for '${term}' :</h2>`;
+
+                if(data.meals === null) {
+                    resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`
+                    mealsEl.innerHTML = ""
+                } else {
+                    mealsEl.innerHTML = data.meals.map(meal => `
+                        <div class="meal">
+                            <img src=${meal.strMealThumb} alt="${meal.strMeal}"/>
+                            <div class="meal-info" data-mealID="${meal.idMeal}">
+                                <h3>${meal.strMeal}</h3>
+                            </div>
+                        </div>
+                `).join("");
+                }
             });
+            // Clear search text
+            search.value = "";
     } else {
         alert("Please enter a search meal");
     }
