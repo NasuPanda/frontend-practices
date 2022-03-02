07ecc7a752cffe7332c43a5f1acff57811b9146f
Westen0511 <xiyuanjunzhi@gmail.com>
2022-03-03 07:40:15 +0900

    Add loading animation

--- New-Year-Countdown/script.js
+++ New-Year-Countdown/script.js
@@ -3,11 +3,17 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
+const loading = document.getElementById("loading");
 
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+// Set background year
+year.innerText = currentYear + 1;
+
+// Update countdown time
 function updateCountdown() {
   const currentTime = new Date();
   const diff = newYearTime - currentTime;
@@ -18,11 +24,18 @@ function updateCountdown() {
   const m = Math.floor(diff / 1000 / 60) % 60;
   const s = Math.floor(diff / 1000) % 60;
 
+  // Add values to DOM
   days.textContent = d
   hours.textContent = String(h).padStart(2, "0");
   minutes.textContent = String(m).padStart(2, "0");
   seconds.textContent = String(s).padStart(2, "0");
-
 }
 
+// show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = "flex";
+}, 1000);
+
+// Run every seconds
 setInterval(updateCountdown, 1000);
