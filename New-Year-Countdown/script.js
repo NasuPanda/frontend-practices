5eb2053ffc921d9f82da7d92f4601e206454b00e
Westen0511 <xiyuanjunzhi@gmail.com>
2022-03-03 07:52:12 +0900

    style: Add comment

--- New-Year-Countdown/script.js
+++ New-Year-Countdown/script.js
@@ -1,13 +1,14 @@
+const countdown = document.getElementById("countdown");
+const loading = document.getElementById("loading");
+const year = document.getElementById("year");
 const days = document.getElementById("days");
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
-const countdown = document.getElementById("countdown");
-const year = document.getElementById("year");
-const loading = document.getElementById("loading");
 
+// Date constructor return ms
+// .getFullYear() return 2022(Number)
 const currentYear = new Date().getFullYear();
-
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
 // Set background year
