2794c9a8104350d5090dfed072b20c2db97a800c
Westen0511 <xiyuanjunzhi@gmail.com>
2022-07-25 08:15:26 +0900

    feat: Add fetch in useEffect to intro to the use case of fetch API in useEffect

--- introduction-to-react/hello-react/src/App.jsx
+++ introduction-to-react/hello-react/src/App.jsx
@@ -1,19 +1,28 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-import Article from "./components/Article"
-import TextInput from './components/TextInput';
-import Counter from "./components/Counter"
-import ToggleButton from './components/ToggleButton';
 
 function App() {
+  const [id, setId] = useState('google')
+  const [name, setName] = useState('')
+  const ids = ['aws', 'google', 'facebook', 'deatiger', 'gaearon']
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random * ids.length)]
+    setId(_id)
+  }
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setName(data.name)
+      })
+      .catch(error => { console.log(error) })
+  }, [id])
   return (
     <div>
-      <Article
-        title={"this is a title"}
-        content={"contents"}
-      />
-      <TextInput></TextInput>
-      <Counter></Counter>
-      <ToggleButton></ToggleButton>
+      <p>{id}, {name}</p>
+      <button onClick={getRandomId}>ID変更</button>
     </div>
   );
 }
