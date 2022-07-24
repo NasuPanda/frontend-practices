10db747b693cb0ea447929fd5470f24ee47a9d4e
Westen0511 <xiyuanjunzhi@gmail.com>
2022-07-24 19:05:02 +0900

    feat: Add useState

--- introduction-to-react/hello-react/src/components/Article.jsx
+++ introduction-to-react/hello-react/src/components/Article.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 const Article = (props) => {
-  return <div>
-    <h2>{props.title}</h2>
-    <p>{props.content}</p>
-  </div>
+  const [isPublished, setIsPublished] = useState(false)
+  console.log(isPublished)
+
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.content}</p>
+      {/* onClickにcallbackを登録 */}
+      <button onClick={() => setIsPublished(true)}>公開</button>
+    </div>
+  )
 };
 
 export default Article
