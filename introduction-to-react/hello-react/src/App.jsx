db59d48142e3bc3023625735d2db4b59f5f5dee1
Westen0511 <xiyuanjunzhi@gmail.com>
2022-07-24 15:02:42 +0900

    feat: Add child components and call it parent component

---
+++ introduction-to-react/hello-react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Article from "./components/Article.jsx"
+
+function App() {
+  return (
+    <div>
+      <Article
+        title={"this is a title"}
+        content={"contents"}
+      />
+    </div>
+  );
+}
+export default App;
---
+++ introduction-to-react/hello-react/src/components/Article.jsx
@@ -0,0 +1,8 @@
+const Article = (props) => {
+  return <div>
+    <h2>{props.title}</h2>
+    <p>{props.content}</p>
+  </div>
+};
+
+export default Article
