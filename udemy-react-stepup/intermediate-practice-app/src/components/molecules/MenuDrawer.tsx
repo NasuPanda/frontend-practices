650aebbca7800ac7ab4b091b4e494926f2fa8006
Westen0511 <xiyuanjunzhi@gmail.com>
2022-09-04 14:09:01 +0900

    Finish 11 Add routing

--- udemy-react-stepup/intermediate-practice-app/src/components/molecules/MenuDrawer.tsx
+++ udemy-react-stepup/intermediate-practice-app/src/components/molecules/MenuDrawer.tsx
@@ -10,20 +10,31 @@ import { memo, VFC } from 'react';
 type Props = {
   onClose: () => void;
   isOpen: boolean;
+  onClickHome: () => void;
+  onClickUserManagement: () => void;
+  onClickSetting: () => void;
 };
 
-const MenuDrawer: VFC<Props> = memo(({ onClose, isOpen }) => (
-  <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
-    <DrawerOverlay>
-      <DrawerContent>
-        <DrawerBody p={0} bg="gray.100">
-          <Button w="100%">TOP</Button>
-          <Button w="100%">ユーザ一覧</Button>
-          <Button w="100%">設定</Button>
-        </DrawerBody>
-      </DrawerContent>
-    </DrawerOverlay>
-  </Drawer>
-));
+const MenuDrawer: VFC<Props> = memo(
+  ({ onClose, isOpen, onClickHome, onClickUserManagement, onClickSetting }) => (
+    <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+      <DrawerOverlay>
+        <DrawerContent>
+          <DrawerBody p={0} bg="gray.100">
+            <Button w="100%" onClick={onClickHome}>
+              TOP
+            </Button>
+            <Button w="100%" onClick={onClickUserManagement}>
+              ユーザ一覧
+            </Button>
+            <Button w="100%" onClick={onClickSetting}>
+              設定
+            </Button>
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+  ),
+);
 
 export default MenuDrawer;
--- udemy-react-stepup/intermediate-practice-app/src/components/organisms/layout/Header.tsx
+++ udemy-react-stepup/intermediate-practice-app/src/components/organisms/layout/Header.tsx
@@ -1,11 +1,24 @@
-import { memo, VFC } from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { memo, useCallback, VFC } from 'react';
 
 import { Flex, Heading, Link, Box, useDisclosure } from '@chakra-ui/react';
+import { useHistory } from 'react-router-dom';
 import MenuIconButton from '../../atoms/button/MenuIconButton';
 import MenuDrawer from '../../molecules/MenuDrawer';
 
 const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const history = useHistory();
+
+  const onClickHome = useCallback(() => history.push('/home'), [history]);
+  const onClickUserManagement = useCallback(
+    () => history.push('/home/user_management'),
+    [history],
+  );
+  const onClickSetting = useCallback(
+    () => history.push('/home/setting'),
+    [history],
+  );
 
   return (
     <>
@@ -18,7 +31,13 @@ const Header: VFC = memo(() => {
         padding={{ base: 3, md: 5 }}
       >
         {/* ヘッダータイトル */}
-        <Flex align="center" as="a" mr={8} _hover={{ cursor: 'pointer' }}>
+        <Flex
+          align="center"
+          as="a"
+          mr={8}
+          _hover={{ cursor: 'pointer' }}
+          onClick={onClickHome}
+        >
           <Heading as="h1" fontSize={{ base: 'md', md: 'lg' }}>
             ユーザ管理アプリ
           </Heading>
@@ -33,16 +52,22 @@ const Header: VFC = memo(() => {
         >
           {/* ヘッダーリンク */}
           <Box pr={4}>
-            <Link href="*">ユーザ一覧</Link>
+            <Link onClick={onClickUserManagement}>ユーザ一覧</Link>
           </Box>
-          <Link href="*">設定</Link>
+          <Link onClick={onClickSetting}>設定</Link>
           {/* ハンバーガーメニュー */}
         </Flex>
         <MenuIconButton onClick={onOpen} />
       </Flex>
 
       {/* 展開可能なメニュー */}
-      <MenuDrawer onClose={onClose} isOpen={isOpen} />
+      <MenuDrawer
+        onClose={onClose}
+        isOpen={isOpen}
+        onClickHome={onClickHome}
+        onClickUserManagement={onClickUserManagement}
+        onClickSetting={onClickSetting}
+      />
     </>
   );
 });
