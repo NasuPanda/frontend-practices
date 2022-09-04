ce48a9d257ed38123d7847ce38da2f0b4d64a636
Westen0511 <xiyuanjunzhi@gmail.com>
2022-09-04 14:07:48 +0900

    hack: Add @ts-ignore to ignore BrowserRouter error

--- udemy-react-stepup/intermediate-practice-app/src/App.tsx
+++ udemy-react-stepup/intermediate-practice-app/src/App.tsx
@@ -9,6 +9,9 @@ import Router from './router/Router';
 const App: FC = () => (
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   <ChakraProvider theme={theme}>
+    {/* HACK: BrowserRouter No overload matches this call. を一時的に黙らせる */}
+    {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+    {/* @ts-ignore */}
     <BrowserRouter>
       <Router />
     </BrowserRouter>
